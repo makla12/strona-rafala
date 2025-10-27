4c38a890ca35fb60bfc9de991bf850a9dede4917
makla <1234makla@gmail.com>
2025-10-27 15:38:55 +0100

    v1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',      // The protocol used (http or https)
+        hostname: 'placehold.co', // The exact domain you need to allow
+        port: '',               // Leave blank unless a specific port is required
+        pathname: '/**',        // Allows any path on that domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
